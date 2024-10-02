e05db8fb6ad2d21dc57424614dacad0803dcb735
danijerez <danieljerezgarrido@gmail.com>
2024-10-02 10:22:06 +0200

    tests

--- src/tests/IntegrationTests/Tests.Audio.cs
+++ src/tests/IntegrationTests/Tests.Audio.cs
@@ -5,10 +5,9 @@ public partial class Tests
     [TestMethod]
     public async Task GenerateTranscriptionAudio()
     {
+        using var client = GetAuthenticatedClient();
         var url = "https://speech.zone/simple4all/wp-content/uploads/2012/10/nina_01_071.wav";
         string tempFilePath = await DownloadFileToTempAsync(url);
-
-        using var client = GetAuthenticatedClient();
 
         var request = new CreateTranscriptionRequest { 
             Filename = "sample", 
